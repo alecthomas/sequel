d9cff02787c35e3675470e1988f756ffbbef4b44
Alec Thomas <alec@swapoff.org>
2019-02-26 23:13:55 +1100

    Split builder out into its own file.

--- db.go
+++ db.go
@@ -167,7 +167,7 @@ func (q *queryable) Insert(table string, rows ...interface{}) (sql.Result, error
 		return nil, errors.Errorf("no rows to insert")
 	}
 	arg, t := q.typeForMutationRows(rows...)
-	builder, err := q.makeRowBuilderForType(t)
+	builder, err := makeRowBuilderForType(t)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to map type %s", t)
 	}
@@ -185,7 +185,7 @@ func (q *queryable) Upsert(table string, rows ...interface{}) (sql.Result, error
 		return nil, errors.Errorf("no rows to update")
 	}
 	arg, t := q.typeForMutationRows(rows...)
-	builder, err := q.makeRowBuilderForType(t)
+	builder, err := makeRowBuilderForType(t)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to map type %s", t)
 	}
@@ -233,7 +233,7 @@ func (q *queryable) Exec(query string, args ...interface{}) (res sql.Result, err
 //
 // The shape and names of the query must match the shape and field names of the slice elements.
 func (q *queryable) Select(slice interface{}, query string, args ...interface{}) (err error) {
-	builder, err := q.makeRowBuilderForSlice(slice)
+	builder, err := makeRowBuilderForSlice(slice)
 	if err != nil {
 		return errors.Wrapf(err, "failed to map slice %T", slice)
 	}
@@ -262,7 +262,7 @@ func (q *queryable) Select(slice interface{}, query string, args ...interface{})
 //
 // Will return sql.ErrNoRows if no rows are returned.
 func (q *queryable) SelectOne(ref interface{}, query string, args ...interface{}) error {
-	builder, err := q.makeRowBuilder(ref)
+	builder, err := makeRowBuilder(ref)
 	if err != nil {
 		return errors.Wrapf(err, "failed to map type %T", ref)
 	}
@@ -340,168 +340,4 @@ func (q *queryable) SelectInt(query string, args ...interface{}) (value int, err
 // SelectString selects a single column row into a string and returns it.
 func (q *queryable) SelectString(query string, args ...interface{}) (value string, err error) {
 	return value, q.SelectScalar(&value, query, args)
-}
-
-// Creates a function that can efficiently construct field references for use with sql.Rows.Scan(...).
-func (q *queryable) makeRowBuilder(v interface{}) (*builder, error) {
-	t := reflect.TypeOf(v)
-	if t.Kind() != reflect.Ptr || t.Elem().Kind() != reflect.Struct {
-		return nil, errors.Errorf("can only scan into pointer to struct, not %s", t)
-	}
-	return q.makeRowBuilderForType(t.Elem())
-}
-
-// Creates a function that can efficiently construct field references for use with sql.Rows.Scan(...).
-func (q *queryable) makeRowBuilderForSlice(slice interface{}) (*builder, error) {
-	t := reflect.TypeOf(slice)
-	if t.Kind() != reflect.Ptr || t.Elem().Kind() != reflect.Slice || t.Elem().Elem().Kind() != reflect.Struct {
-		return nil, errors.Errorf("expected a pointer to a slice of structs but got %T", slice)
-	}
-	t = t.Elem().Elem()
-	return q.makeRowBuilderForType(t)
-}
-
-func (q *queryable) makeRowBuilderForType(t reflect.Type) (*builder, error) {
-	if t.Kind() != reflect.Struct {
-		return nil, errors.Errorf("can only build rows for structs not %s", t)
-	}
-	rowBuilderLock.Lock()
-	defer rowBuilderLock.Unlock()
-	if builder, ok := rowBuilderCache[t]; ok {
-		return builder, nil
-	}
-
-	fields, err := q.collectFieldIndexes(t)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to collect field indexes")
-	}
-	fieldMap := map[string]field{}
-	fieldNames := []string{}
-	pk := ""
-	for _, field := range fields {
-		if field.pk {
-			pk = field.name
-		}
-		fieldNames = append(fieldNames, field.name)
-		fieldMap[field.name] = field
-	}
-	return &builder{pk: pk, t: t, fields: fieldNames, fieldMap: fieldMap}, nil
-}
-
-func (q *queryable) collectFieldIndexes(t reflect.Type) ([]field, error) {
-	out := []field{}
-	for i := 0; i < t.NumField(); i++ {
-		f := t.Field(i)
-		ft := f.Type
-
-		if ft == timeType || ft == byteSliceType || ft.Implements(scannerType) || reflect.PtrTo(ft).Implements(scannerType) {
-			name, pk := parseField(f)
-			out = append(out, field{
-				name:  name,
-				pk:    pk,
-				index: []int{i},
-			})
-			continue
-		}
-
-		switch ft.Kind() {
-		case reflect.Struct:
-			if !f.Anonymous {
-				return nil, errors.Errorf("struct field \"%s %s\" must implement sql.Scanner to be mapped to a field", f.Name, ft)
-			}
-			sub, err := q.collectFieldIndexes(ft)
-			if err != nil {
-				return nil, err
-			}
-			for _, field := range sub {
-				field.index = append([]int{i}, field.index...)
-				out = append(out, field)
-			}
-
-		case reflect.Slice, reflect.Array:
-			return nil, errors.Errorf("can't select into slice field \"%s %s\"", f.Name, ft)
-
-		default:
-			name, pk := parseField(f)
-			out = append(out, field{
-				name:  name,
-				pk:    pk,
-				index: []int{i},
-			})
-		}
-	}
-
-	return out, nil
-}
-
-func parseField(f reflect.StructField) (name string, pk bool) {
-	name = strings.ToLower(f.Name)
-	tag, ok := f.Tag.Lookup("db")
-	if !ok {
-		return
-	}
-	parts := strings.Split(tag, ",")
-	if parts[0] != "" {
-		name = parts[0]
-	}
-	for _, part := range parts[1:] {
-		if part == "pk" {
-			pk = true
-		}
-	}
-	return
-}
-
-type field struct {
-	name  string
-	pk    bool
-	index []int
-}
-
-type builder struct {
-	pk       string
-	t        reflect.Type
-	fields   []string
-	fieldMap map[string]field
-}
-
-func (b *builder) fill(v interface{}, columns []string) (out []interface{}) {
-	rv := reflect.ValueOf(v).Elem()
-	out = make([]interface{}, len(b.fields))
-	for i, column := range columns {
-		out[i] = rv.FieldByIndex(b.fieldMap[column].index).Addr().Interface()
-	}
-	return
-}
-
-func (b *builder) build(columns []string, types []*sql.ColumnType) (reflect.Value, []interface{}) {
-	out := make([]interface{}, len(columns))
-	v := reflect.New(b.t).Elem()
-	for i, column := range columns {
-		field, ok := b.fieldMap[column]
-		if ok {
-			out[i] = v.FieldByIndex(field.index).Addr().Interface()
-			continue
-		}
-
-		// Should only hit this in unsafe mode.
-		switch types[i].DatabaseTypeName() {
-		case "VARCHAR", "TEXT", "NVARCHAR", "STRING", "CHARACTER", "VARYING CHARACTER", "NCHAR", "NATIVE CHARACTER", "CLOB":
-			out[i] = new(string)
-		case "NUMERIC", "DECIMAL", "INT", "BIGINT", "INTEGER", "TINYINT", "SMALLINT", "MEDIUMINT", "UNSIGNED BIG INT", "INT2", "INT8":
-			out[i] = new(int64)
-		case "REAL", "FLOAT", "DOUBLE", "DOUBLE PRECISION":
-			out[i] = new(float64)
-		case "BOOL":
-			out[i] = new(bool)
-		case "DATE", "DATETIME":
-			out[i] = &time.Time{}
-		case "BYTE":
-			b := []byte{}
-			out[i] = &b
-		default:
-			panic("unsupported missing field type " + types[i].DatabaseTypeName())
-		}
-	}
-	return v, out
 }
